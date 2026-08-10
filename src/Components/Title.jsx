f1c6714769cff3890849fd50ba515203e9bd9e2f
prathmyx <pratham.yadav.gr8@outlook.com>
2026-08-10 10:43:25 +0530

    feat:added title

---
+++ src/Components/Title.jsx
@@ -0,0 +1,9 @@
+export default function Title() {
+    return (
+        <div className="title-container">
+            <h1 className="title-head">Assembly: Endgame</h1>
+            <p className="title-para">Guess the word within 8 attempts to keep the 
+            programming world safe from Assembly!</p>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { createRoot } from 'react-dom/client'
+import Title from './Components/Title.jsx'
 import App from './Components/App.jsx'
 
 createRoot(document.getElementById('root'))
     .render(
-        <App />
+        <>
+            <Title />
+            <App />
+        </>
 )
